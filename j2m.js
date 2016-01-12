aecf9526402b92c0fb794193607d7bdb905e595c
Alex Dergachev <alex@evolvingweb.ca>
2016-01-12 14:41:48 -0500

    Committing clean version of j2m.js; from https://github.com/kylefarris/J2M/blob/master/index.js

--- j2m.js
+++ j2m.js
@@ -1,6 +1,62 @@
-// extracted from https://github.com/kylefarris/J2M/blob/master/index.js
+var marked = require('marked');
+marked.setOptions({
+	breaks: true,
+	smartyPants: true
+});
 
-function md2jira(md_str) {
+var J2M = function() {};
+
+J2M.prototype.md_to_html = function(str) {
+	return marked(str);
+}
+
+J2M.prototype.jira_to_html = function(str) {
+	return marked(this.to_markdown(str));
+}
+
+J2M.prototype.to_markdown = function(str) {
+    return str
+        // Ordered Lists
+        .replace(/^[ \t]*(\*+)\s+/gm, function(match, stars) {
+            return Array(stars.length).join(" ") + '* ';
+        })
+        // Un-ordered lists
+        .replace(/^[ \t]*(#+)\s+/gm, function(match, nums) {
+            return Array(nums.length).join(" ") + '1. ';
+        })
+        // Headers 1-6
+        .replace(/^h([0-6])\.(.*)$/gm, function (match, level, content) {
+            return Array(parseInt(level) + 1).join('#') + content;
+        })
+        // Bold
+        .replace(/\*(\S.*)\*/g, '**$1**')
+        // Italic
+        .replace(/\_(\S.*)\_/g, '*$1*')
+        // Monospaced text
+        .replace(/\{\{([^}]+)\}\}/g, '`$1`')
+        // Citations
+        .replace(/\?\?((?:.[^?]|[^?].)+)\?\?/g, '<cite>$1</cite>')
+        // Inserts
+        .replace(/\+([^+]*)\+/g, '<ins>$1</ins>')
+        // Superscript
+        .replace(/\^([^^]*)\^/g, '<sup>$1</sup>')
+        // Subscript
+        .replace(/~([^~]*)~/g, '<sub>$1</sub>')
+        // Strikethrough
+        .replace(/-(\S+.*?\S)-/g, '~~$1~~')
+        // Code Block
+        .replace(/\{code(:([a-z]+))?\}([^]*)\{code\}/gm, '```$2$3```')
+        // Pre-formatted text
+        .replace(/{noformat}/g, '```')
+        // Un-named Links
+        .replace(/\[([^|]+)\]/g, '<$1>')
+        // Named Links
+        .replace(/\[(.+?)\|(.+)\]/g, '[$1]($2)')
+        // Single Paragraph Blockquote
+        .replace(/^bq\.\s+/gm, '> ')
+};
+
+J2M.prototype.to_jira = function(str) {
     var map = {
       cite: '??',
       del: '-',
@@ -9,7 +65,7 @@ function md2jira(md_str) {
       sub: '~'
     };
 
-    return md_str
+    return str
         // Bold, Italic, and Combined (bold+italic)
         .replace(/([*_]+)(\S.*?)\1/g, function (match,wrapper,content) {
             switch (wrapper.length) {
